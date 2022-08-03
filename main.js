db263ee562afef82a59014e5614db641c02288d2
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2022-08-03 10:59:42 -0400

    add working saerch  bar

--- main.js
+++ main.js
@@ -3,17 +3,32 @@ console.log('connected!')
 let resultsDiv = document.querySelector('#results')
 console.log('results div', resultsDiv)
 
-let searchUrl = 'https://api.artic.edu/api/v1/artworks?page=2&limit=100'
 
-fetch(searchUrl, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' }
+
+let searchBaseUrl = 'https://api.artic.edu/api/v1/artworks/search?q='
+
+let searchForm = document.querySelector('#search-form')
+
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let searchBox = document.querySelector('#search-box')
+    let searchUrl = `${searchBaseUrl}${searchBox.value}`
+    console.log('search url', searchUrl)
+    getSearchResults(searchUrl)
 })
-    // response is whatever the fetch returns
-    .then(function (response) {
-        return response.json()
+
+function getSearchResults(url) {
+    fetch(url, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
     })
-    // data is whatever the above code returns, in this case response.json()
-    .then(function (data) {
-        console.log(data.data)
-    })
+        // response is whatever the fetch returns
+        .then(function (response) {
+            return response.json()
+        })
+        // data is whatever the above code returns, in this case response.json()
+        .then(function (data) {
+            console.log(data.data)
+        })
+
+}
